bdf808faedebaa10b6525510d0f40c99bf451716
jestin <jestinoommen2@gmail.com>
2023-02-08 20:06:43 +0530

    Completes

---
+++ calculator.py
@@ -0,0 +1,10 @@
+# Create a calculator capable of performing addition,subtraction,multiplication
+#and division operation on two number
+
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+
+print("Addition of two numbers is",a+b)
+print("Subtraction of two numbers is",a-b)
+print("Multiplication of two numbers is",a*b)
+print("Division of two numbers is",a/b)
